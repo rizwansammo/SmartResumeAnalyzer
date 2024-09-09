75b599e75ce10e9875b3cba4f82dd19319964cdd
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-09 22:28:32 +0600

    Routes

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,18 @@
 from flask import Blueprint, render_template, request
+from .resume_analyzer import analyze_resume
+from .ats_checker import check_ats
+from .grammar_checker import check_grammar
+
+main = Blueprint('main', __name__)
+
+@main.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        resume_file = request.files['resume']
+        feedback = analyze_resume(resume_file)
+        ats_score = check_ats(resume_file)
+        grammar_feedback = check_grammar(resume_file)
+        return render_template('result.html', feedback=feedback, ats_score=ats_score, grammar_feedback=grammar_feedback)
+    return render_template('upload.html')
+
 
