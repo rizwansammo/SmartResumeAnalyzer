29055f9a63af107047cfb827807df51d4f21c0d7
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-09 22:30:52 +0600

    Grammar Checker

--- app/grammar_checker.py
+++ app/grammar_checker.py
@@ -1,1 +1,7 @@
-# This is the grammar_checker.py file
+import language_tool_python
+
+def check_grammar(file):
+    tool = language_tool_python.LanguageTool('en-US')
+    text = file.read().decode('utf-8')
+    matches = tool.check(text)
+    return {"grammar_errors": len(matches), "suggestions": matches}
