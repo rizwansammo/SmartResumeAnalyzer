cd8c8b0a40260ab1debf7c86f94f9eea43721f3a
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-09 22:30:03 +0600

    ATS Checker

--- app/ats_checker.py
+++ app/ats_checker.py
@@ -1,1 +1,13 @@
-# This is the ats_checker.py file
+import PyPDF2
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+def analyze_resume(file):
+    reader = PyPDF2.PdfReader(file)
+    text = ''
+    for page in reader.pages:
+        text += page.extract_text()
+    doc = nlp(text)
+    skills = [ent.text for ent in doc.ents if ent.label_ == 'SKILL']
+    return {"skills": skills}
