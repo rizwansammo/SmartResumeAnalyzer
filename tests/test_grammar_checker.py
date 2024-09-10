55d1d4299d5b49f1f0a1b2bcff0250b9104408f1
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-10 19:53:03 +0600

    Grammar Checker

--- tests/test_grammar_checker.py
+++ tests/test_grammar_checker.py
@@ -1,1 +1,14 @@
-# This is the test_grammar_checker.py file
+import unittest
+from app.grammar_checker import check_grammar
+from io import BytesIO
+
+class TestGrammarChecker(unittest.TestCase):
+    def test_check_grammar(self):
+        dummy_text = "This is a sentence with bad grammar This is another one"
+        dummy_file = BytesIO(dummy_text.encode('utf-8'))
+        result = check_grammar(dummy_file)
+        self.assertGreater(result['grammar_errors'], 0)
+        self.assertIsInstance(result['suggestions'], list)
+
+if __name__ == "__main__":
+    unittest.main()
